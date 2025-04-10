5d38c8fd235568751a156ebff45a7145e27045e4
AlekseiPodusko <112948029+AlekseiPodusko@users.noreply.github.com>
2025-04-10 22:35:25 +0200

    Add files via upload

---
+++ GameCodes/build/generated-sources/ap-source-output/entity/Picture_.java
@@ -0,0 +1,14 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-04-10T23:32:37")
+@StaticMetamodel(Picture.class)
+public class Picture_ { 
+
+    public static volatile SingularAttribute<Picture, Long> pictureid;
+    public static volatile SingularAttribute<Picture, String> pathToFile;
+
+}
