51082504502a15627b534c9801a3352fde1b767d
AlekseiPodusko <112948029+AlekseiPodusko@users.noreply.github.com>
2025-04-10 23:29:47 +0200

    Add files via upload

---
+++ GameCodes/build/generated-sources/ap-source-output/entity/Code_.java
@@ -0,0 +1,18 @@
+package entity;
+
+import entity.Product;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-04-11T00:22:55")
+@StaticMetamodel(Code.class)
+public class Code_ { 
+
+    public static volatile SingularAttribute<Code, Product> game;
+    public static volatile SingularAttribute<Code, String> code;
+    public static volatile SingularAttribute<Code, String> salt;
+    public static volatile SingularAttribute<Code, String> purchase;
+    public static volatile SingularAttribute<Code, Long> id;
+
+}
---
+++ GameCodes/build/generated-sources/ap-source-output/entity/History_.java
@@ -0,0 +1,21 @@
+package entity;
+
+import entity.Code;
+import entity.Product;
+import entity.User;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-04-11T00:22:55")
+@StaticMetamodel(History.class)
+public class History_ { 
+
+    public static volatile SingularAttribute<History, Date> purchaseDate;
+    public static volatile SingularAttribute<History, Product> product;
+    public static volatile SingularAttribute<History, Code> code;
+    public static volatile SingularAttribute<History, Long> id;
+    public static volatile SingularAttribute<History, User> user;
+
+}
