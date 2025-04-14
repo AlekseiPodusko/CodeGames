742dffdd09246daa154d518cb81f347108204870
AlekseiPodusko <112948029+AlekseiPodusko@users.noreply.github.com>
2025-04-14 07:47:31 +0200

    Add files via upload

---
+++ GameCodes/build/generated-sources/ap-source-output/entity/Picture_.java
@@ -0,0 +1,14 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-04-14T08:33:02")
+@StaticMetamodel(Picture.class)
+public class Picture_ { 
+
+    public static volatile SingularAttribute<Picture, Long> pictureid;
+    public static volatile SingularAttribute<Picture, String> pathToFile;
+
+}
---
+++ GameCodes/build/generated-sources/ap-source-output/entity/User_.java
@@ -0,0 +1,23 @@
+package entity;
+
+import entity.Product;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-04-14T08:33:02")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> firstName;
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> salt;
+    public static volatile ListAttribute<User, Product> listProducts;
+    public static volatile SingularAttribute<User, Long> id;
+    public static volatile SingularAttribute<User, String> login;
+    public static volatile SingularAttribute<User, String> sureName;
+    public static volatile SingularAttribute<User, Double> cash;
+    public static volatile SingularAttribute<User, String> email;
+
+}
